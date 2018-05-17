d683d46c4e1c075435508e0fb9921cf266fc2235
zfei@ucdavis.edu <zfei@ucdavis.edu>
2018-05-17 14:28:00 -0700

    gps/odom, tf tree

---
+++ publish_gps_odom.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python  
+import roslib
+import rospy
+import utm
+import numpy as np
+import tf
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Pose, Point, Quaternion
+from sensor_msgs.msg import NavSatFix
+import math
+
+pub = rospy.Publisher('/gps/pose', Odometry, queue_size=1)
+init_pose = [6.05213442e+05, 4.26608607e+06, 1.52632201e+09]
+
+def publish_pose(msg):
+
+    gps_odom = Odometry()   
+    gps_odom.header.stamp = msg.header.stamp
+    gps_odom.header.frame_id = "earth"
+    utm_coord = utm.from_latlon(msg.latitude, msg.longitude)
+    # odom_quat = tf.transformations.quaternion_from_euler(0, 0, 0.93863332 + math.pi/4)
+    odom_quat = tf.transformations.quaternion_from_euler(0, 0, 0.93863332 + math.pi/4)
+
+    gps_odom.pose.pose = Pose(Point(utm_coord[0] - init_pose[0], utm_coord[1] - init_pose[1], 0.), Quaternion(*odom_quat))
+    rospy.loginfo(gps_odom)
+    pub.publish(gps_odom)
+
+
+if __name__ == '__main__':
+    rospy.init_node('gps_odom')
+    rospy.Subscriber('/gps0/fix', NavSatFix, publish_pose)
+    rospy.spin()
--- publish_tf.py
+++ publish_tf.py
@@ -4,14 +4,27 @@ import rospy
 
 import tf
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Pose, Point, Quaternion
+import math
+
+yaw = 0.93863332 + math.pi/4
+q_init = tf.transformations.quaternion_from_euler(0, 0, yaw)
 
 def cam_pose(msg):
     br = tf.TransformBroadcaster()
-    br.sendTransform( (msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z ),
+    q_now = tf.transformations.quaternion_multiply([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w], q_init)
+   
+    br.sendTransform((msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z ),
                       (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w),
                       msg.header.stamp,
                      "zed_left_camera",
                      "map")
+
+    br.sendTransform( (0 , 0,  0, 0),
+                      (q_init[0], q_init[1], q_init[2], q_init[3]),
+                      msg.header.stamp,
+                     "map",
+                     "earth")
 
 if __name__ == '__main__':
     rospy.init_node('turtle_tf_broadcaster')
